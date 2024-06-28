014e889ffbc3185676c785eab922ca4fc158b0e1
Михаил Лубенцов <wispergently061@gmail.com>
2024-06-28 22:35:35 +0500

    Добавил много новых понятий из ООП в этот проект, в частности методы default в интерфейсах, так же добавил парктику лямбда выражений с методами из интерфесов.

--- OOP/Swimable.java
+++ OOP/Swimable.java
@@ -2,14 +2,23 @@ package OOP;
 
 import static print_module.print_library.println;
 
+
+
 /*
  * Интерфейс это ссылочный тип данных, который не имеет состояний (полей класса), но имеет только поведения (мметоды класса)
  */
 public interface Swimable {
+    static String type = "Swimable";
+    
     void swim(float speed); // По умолчанию у интерфейсов все методы public abstract, поэтому это не обязательно указывать.
 
     // метод default можно уже заранее описать в интерфесе. и он будет одинаковый для всех наследников этого метода.
+    // ключевое слово default исключительно для интерфейсов, в классах abstract не используется, потому что там можно реализовывать логику метода уже изначально.
     default void swim(){
         println("Плывет со скоростью 5");
+    }
+
+    default String getType(){
+        return type;
     }
 }
--- OOP/Task1.java
+++ OOP/Task1.java
@@ -1,5 +1,7 @@
 package OOP;
 import static print_module.print_library.println;
+import java.util.List;
+import java.util.ArrayList;
 /**
  * Task1
  */
@@ -23,10 +25,33 @@ public class Task1 {
         duck3.swim(12.5f);
         duck3.swim();
         
-        duck2.swim(13.6f);// duck2 объявлен как Swimmable, поэтому у него нет методов eat и fly.
+        duck2.swim(13.6f); // duck2 объявлен как Swimmable, поэтому у него нет методов eat и fly.
 
         cat.eat();
         cat.swim(3.5f);
+
+        Animal a = new Animal("Animal") {
+
+            @Override
+            public void eat() {
+                println("Кушает");
+            }
+            
+        };
+
+        Swimable swimable = new Swimable() {
+
+            @Override
+            public void swim(float speed) {
+                println(speed / 2);
+            }
+            
+        };
+
+        Swimable swimable2 = (speed -> println(speed * speed));
+
+        swimable2.swim(12f);
+        swimable.swim(12f);
 
         
     }
