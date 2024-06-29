75a618ac3a7e99cfcc8f9bf8c775938ac646f2b8
Михаил Лубенцов <wispergently061@gmail.com>
2024-06-29 19:57:45 +0500

    Мелкие исправления кода

--- OOP/Market/MarketBehavoir.java
+++ OOP/Market/MarketBehavoir.java
@@ -1,7 +1,5 @@
 package OOP.Market;
 
-import java.util.List;
-
 public interface MarketBehavoir {
     void acceptToMarket(Actor actor); // входит в магазин
     void releaseFromMarket(Actor actor); // выходит из магазина
